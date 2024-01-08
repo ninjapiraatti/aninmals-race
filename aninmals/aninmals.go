19273e8f8b390615bee1a96706ae016817f450c2
ninjapiraatti <tuoppil@gmail.com>
2024-01-08 14:29:02 +0200

    Add visualization of the race

--- aninmals/aninmals.go
+++ aninmals/aninmals.go
@@ -4,20 +4,33 @@ import (
 	"math/rand"
 )
 
-type Aninmal struct {
-	Name string
-}
-
 var (
 	adjectives   = []string{"Loving", "Timid", "Furious", "Shiny", "Mechanical", "Pissed", "Cuddly"}
 	aninmalNames = []string{"Treefloof", "Murder Mittens", "Patience Monkey", "Forest Gorgi", "Wizard Cow", "Formal Chikcen"}
 )
 
+type Aninmal struct {
+	Name     string
+	Progress int
+	Color    string
+}
+
+func generateColor() string {
+	return "\033[31m" // Red
+}
+
+func (a *Aninmal) Race() {
+	advance := rand.Intn(3)
+	a.Progress += advance
+}
+
 func Create() Aninmal {
 	adjective := adjectives[rand.Intn(len(adjectives))]
 	name := aninmalNames[rand.Intn(len(aninmalNames))]
 	aninmal := Aninmal{
-		Name: adjective + " " + name,
+		Name:     adjective + " " + name,
+		Progress: 0,
+		Color:    generateColor(),
 	}
 	return aninmal
 }
--- cmd/race/main.go
+++ cmd/race/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/ninjapiraatti/aninmals-race/aninmals"
 )
 
 const numberOfContestants = 3
+const raceLength = 20
+
+func DisplayRace(aninmals []aninmals.Aninmal) {
+	if numberOfContestants > 0 {
+		fmt.Printf("\033[%dA", numberOfContestants)
+	}
+
+	for _, a := range aninmals {
+		progressBar := a.Color + strings.Repeat("#", a.Progress) + strings.Repeat(".", raceLength-a.Progress) + "\033[0m"
+		fmt.Printf("%-15s [%-10s]\n", a.Name, progressBar)
+	}
+}
 
 func main() {
+	fmt.Printf("\n\n\n\n")
+	raceOver := false
+	var mu sync.Mutex
+
 	uniqueAninmals := make([]aninmals.Aninmal, numberOfContestants)
 	for i := 0; i < numberOfContestants; {
 		newAninmal := aninmals.Create()
@@ -17,5 +36,29 @@ func main() {
 			i++
 		}
 	}
-	log.Println(uniqueAninmals)
+
+	var wg sync.WaitGroup
+	for i := range uniqueAninmals {
+		wg.Add(1)
+		go func(a *aninmals.Aninmal) {
+			defer wg.Done()
+			for {
+				mu.Lock()
+				if raceOver {
+					mu.Unlock()
+					break
+				}
+				a.Race()
+				DisplayRace(uniqueAninmals)
+				if a.Progress >= raceLength {
+					raceOver = true
+					fmt.Printf("%s won the race!\n", a.Name)
+				}
+				mu.Unlock()
+				time.Sleep(100 * time.Millisecond)
+			}
+		}(&uniqueAninmals[i])
+	}
+
+	wg.Wait()
 }
