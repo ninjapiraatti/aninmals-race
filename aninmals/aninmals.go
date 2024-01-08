97d063b996fc4aaad44d598d745f87503c899bb5
ninjapiraatti <tuoppil@gmail.com>
2024-01-08 11:59:32 +0200

    Add methods for aninmal creation and duplicate checking

--- aninmals/aninmals.go
+++ aninmals/aninmals.go
@@ -1,5 +1,32 @@
 package aninmals
 
+import (
+	"math/rand"
+)
+
 type Aninmal struct {
 	Name string
+}
+
+var (
+	adjectives   = []string{"Loving", "Timid", "Furious", "Shiny", "Mechanical", "Pissed", "Cuddly"}
+	aninmalNames = []string{"Treefloof", "Murder Mittens", "Patience Monkey", "Forest Gorgi", "Wizard Cow", "Formal Chikcen"}
+)
+
+func Create() Aninmal {
+	adjective := adjectives[rand.Intn(len(adjectives))]
+	name := aninmalNames[rand.Intn(len(aninmalNames))]
+	aninmal := Aninmal{
+		Name: adjective + " " + name,
+	}
+	return aninmal
+}
+
+func IsDuplicate(aninmal Aninmal, aninmalList []Aninmal) bool {
+	for _, a := range aninmalList {
+		if aninmal.Name == a.Name {
+			return true
+		}
+	}
+	return false
 }
--- cmd/race/main.go
+++ cmd/race/main.go
@@ -1,12 +1,21 @@
 package main
 
-import {
+import (
 	"log"
+
 	"github.com/ninjapiraatti/aninmals-race/aninmals"
-}
-	
+)
+
+const numberOfContestants = 3
 
 func main() {
-	uniqueAninmals := make(map[aninmals.Aninmal]bool)
-	log.Println("henlo")
+	uniqueAninmals := make([]aninmals.Aninmal, numberOfContestants)
+	for i := 0; i < numberOfContestants; {
+		newAninmal := aninmals.Create()
+		if !aninmals.IsDuplicate(newAninmal, uniqueAninmals) {
+			uniqueAninmals[i] = newAninmal
+			i++
+		}
+	}
+	log.Println(uniqueAninmals)
 }
